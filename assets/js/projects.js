50bc6458c5bca3f06b6476f2057b249151e27848
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-25 23:23:51 -0300

    [refactor] - creating the project boxes with a better method, using a json file

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,46 +1,49 @@
+import listProjects from "../json/projects.json" assert { type: "json" };
+const projectsDiv = document.querySelector(".projects-div");
+
 const projectsDescription = document.querySelector("#projects-description");
-const boxesAll = document.querySelectorAll("#project-box");
 
-boxesAll.forEach(box => {
-    box.addEventListener("mouseover", (event) => {
-        event.preventDefault;   
+listProjects.forEach(project => {
+    let mainDiv = document.createElement("div");
+    mainDiv.className = "project-box";
+    mainDiv.id = "project-box";
 
-        let title = box.querySelector(".project-box-texts").querySelector("h3").textContent.replaceAll(" ", "");
-        switch(title){
-            case "SphynxV2":
-                projectsDescription.innerHTML = "Continuation of the Sphynx project, focusing on improving and adding features to the original project."
-                break;
-            
-            case "Sphynx":
-                projectsDescription.innerHTML = "Project with the objective of creating a simple and practical security system, using embedded systems and web servers."
-                break;
-            
-            case "Socius":
-                projectsDescription.innerHTML = "Terminal application aiming to help club owners better control their business.";
-                break;
-            
-            case "AluraSpring":
-                projectsDescription.innerHTML = "Project developed during an introduction course to the Spring Framework on the Alura platform, focusing on creating a simple CRUD.";
-                break;
-            
-            case "NewGOATV2":
-                projectsDescription.innerHTML = "Project that aims to create an inventory management system.";
-                break;
+    let image = document.createElement("img");
+    image.src = project.imageUrl;
+    
+    let textDiv = document.createElement("div");
+    textDiv.className = "project-box-texts";
 
-            case "LifeSearch":
-                projectsDescription.innerHTML = "Project with the aim of creating a forum for the dissemination of technology-related projects";
-                break;
+    let title = document.createElement("h3");
+    title.textContent = project.title;
 
-            case "Statilator":
-                projectsDescription.innerHTML = "Simple project developed with the intention of being a statistical calculator.";
-                break;
-        }   
-        projectsDescription.classList.toggle("show");      
+    let subtitle = document.createElement("p");
+    subtitle.textContent = project.subtitle;
+
+    textDiv.appendChild(title);
+    textDiv.appendChild(subtitle);
+
+    let linkButton = document.createElement("a");
+    linkButton.href = project.repository;
+
+    let button = document.createElement("button");
+    button.innerHTML = "See more";
+
+    linkButton.appendChild(button);
+
+    mainDiv.appendChild(image);
+    mainDiv.appendChild(textDiv);
+    mainDiv.appendChild(linkButton);
+
+    projectsDiv.appendChild(mainDiv);
+
+    mainDiv.addEventListener("mouseover", () => {
+        projectsDescription.textContent = project.text;
+        projectsDescription.classList.toggle("show");
     })
 
-    box.addEventListener("mouseout", (event) => {
-        event.preventDefault;
+    mainDiv.addEventListener("mouseout", () => {
         projectsDescription.textContent = "";
         projectsDescription.classList.toggle("show");
     })
-});
+})
