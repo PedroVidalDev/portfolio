e3dc2a715e632b1d147d1c4fc000d2780bf7047d
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-25 21:49:30 -0300

    [refactor] - altering the skills description variable name

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,5 +1,5 @@
 const boxes = document.querySelectorAll(".language-box")
-const description = document.querySelector("#skills-description");
+const skillsDescription = document.querySelector("#skills-description");
 
 boxes.forEach(box => {
     box.addEventListener("mouseover", () => {
@@ -8,35 +8,35 @@ boxes.forEach(box => {
         
         switch(language){
             case "html":
-                description.textContent = "HTML is the standard markup language for creating Web pages.";
+                skillsDescription.textContent = "HTML is the standard markup language for creating Web pages.";
                 break;
             case "css":
-                description.textContent = "CSS is a computer language for laying out and structuring web pages.";
+                skillsDescription.textContent = "CSS is a computer language for laying out and structuring web pages.";
                 break;
             case "js":
-                description.textContent = "JavaScript is a scripting language that enables you to create dynamically updating content.";
+                skillsDescription.textContent = "JavaScript is a scripting language that enables you to create dynamically updating content.";
                 break;
             case "java":
-                description.textContent = "Java is a object-oriented language that can be used as a platform in itself.";
+                skillsDescription.textContent = "Java is a object-oriented language that can be used as a platform in itself.";
                 break;
             case "spring":
-                description.textContent = "Spring is a Java framework created with the aim of facilitating application development.";
+                skillsDescription.textContent = "Spring is a Java framework created with the aim of facilitating application development.";
                 break;
             case "python":
-                description.textContent = "Python is a programming language widely used in web applications, software development and more.";
+                skillsDescription.textContent = "Python is a programming language widely used in web applications, software development and more.";
                 break;
             case "mysql":
-                description.textContent = "MySQL is a database management system that uses the SQL language as an interface.";
+                skillsDescription.textContent = "MySQL is a database management system that uses the SQL language as an interface.";
                 break;
             case "postgre":
-                description.textContent = "PostgreSQL is an object-relational database management system, developed as an open source project.";
+                skillsDescription.textContent = "PostgreSQL is an object-relational database management system, developed as an open source project.";
                 break;
         }
-        description.classList.toggle("show");
+        skillsDescription.classList.toggle("show");
     })
 
     box.addEventListener("mouseout", () => {
-        description.textContent = "";
-        description.classList.toggle("show");
+        skillsDescription.textContent = "";
+        skillsDescription.classList.toggle("show");
     })
 });
