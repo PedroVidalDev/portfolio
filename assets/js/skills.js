a1af178d4dd38e04e76da214459c08a70aa11719
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-25 23:03:03 -0300

    [refactor] - further improving the skill boxes creation

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,6 +1,8 @@
 import listSkills from "../json/skills.json" assert { type: "json" };
 const languagesDiv = document.querySelector(".languages-div");
 
+const skillsDescription = document.querySelector("#skills-description");
+
 listSkills.forEach(skill => {
     let mainDiv = document.createElement("div");
     mainDiv.id = skill.id;
@@ -13,47 +15,14 @@ listSkills.forEach(skill => {
     mainDiv.appendChild(image);
 
     languagesDiv.appendChild(mainDiv);
-})
 
-const boxes = document.querySelectorAll(".language-box");
-const skillsDescription = document.querySelector("#skills-description");
-
-boxes.forEach(box => {
-    box.addEventListener("mouseover", () => {
-        let id = box.id;
-        let language = id.replace("-box", "");
-        
-        switch(language){
-            case "html":
-                skillsDescription.textContent = "HTML is the standard markup language for creating Web pages.";
-                break;
-            case "css":
-                skillsDescription.textContent = "CSS is a computer language for laying out and structuring web pages.";
-                break;
-            case "js":
-                skillsDescription.textContent = "JavaScript is a scripting language that enables you to create dynamically updating content.";
-                break;
-            case "java":
-                skillsDescription.textContent = "Java is a object-oriented language that can be used as a platform in itself.";
-                break;
-            case "spring":
-                skillsDescription.textContent = "Spring is a Java framework created with the aim of facilitating application development.";
-                break;
-            case "python":
-                skillsDescription.textContent = "Python is a programming language widely used in web applications, software development and more.";
-                break;
-            case "mysql":
-                skillsDescription.textContent = "MySQL is a database management system that uses the SQL language as an interface.";
-                break;
-            case "postgre":
-                skillsDescription.textContent = "PostgreSQL is an object-relational database management system, developed as an open source project.";
-                break;
-        }
+    mainDiv.addEventListener("mouseover", () => {
+        skillsDescription.textContent = skill.text;
         skillsDescription.classList.toggle("show");
     })
 
-    box.addEventListener("mouseout", () => {
+    mainDiv.addEventListener("mouseout", () => {
         skillsDescription.textContent = "";
         skillsDescription.classList.toggle("show");
     })
-});
+})
