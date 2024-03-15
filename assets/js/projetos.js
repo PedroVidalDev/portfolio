4b14a556122c315af7c9f095d03f371551005b10
PedroVidalDev <pedrohvidals@gmail.com>
2024-03-15 13:16:45 -0300

    [feat] - adicionando imagens aos projetos

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,3 +1,4 @@
+// MENU //
 $('.sub-btn').next('.sub-menu').slideToggle();
 
 $(document).ready(function(){
@@ -5,3 +6,35 @@ $(document).ready(function(){
         $(this).children('.sub-menu').slideToggle();
     })
 })
+
+// IMAGEM //
+const divImagem = document.querySelector(".projeto-tela");
+const projetos = document.querySelectorAll(".projeto-box");
+
+projetos.forEach(projeto => {
+    projeto.addEventListener("mouseover", () => {
+        let nome = projeto.querySelector(".sub-btn").innerHTML;
+        let nomeFormatado = nome.trim();
+
+        let img = document.createElement("img");
+        img.className = "projeto-img";
+
+        if(nomeFormatado == "Sphynx - Segurança Escolar"){
+            img.src = "/assets/img/projetos/sphynx.jpg";
+        }
+        if(nomeFormatado == "Socius - Gerência de Clubes"){
+            img.src = "/assets/img/projetos/socius.jpg";
+        }
+        if(nomeFormatado == "Picpay Simplificado"){
+            img.src = "/assets/img/projetos/picpay.webp";
+        }
+        if(nomeFormatado == "Livraria Alura"){
+            img.src = "/assets/img/projetos/livraria.jpg";
+        }
+        divImagem.appendChild(img);
+    });
+
+    projeto.addEventListener("mouseout", () => {
+        divImagem.innerHTML = "";
+    })
+});
