9f8ffd8777c31a7e10b1e9e92976637373b39512
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-05 08:40:22 -0300

    feat: mudando formacoes de acordo com os dois botoes

--- assets/js/formacoes.js
+++ assets/js/formacoes.js
@@ -3,27 +3,60 @@ import listaFormacoes from "../json/formacoes.json" assert {type: "json"};
 const formacaoContainerAjuste = document.querySelector("#formacoes-container-ajuste");
 const botoesFormacoes = document.querySelectorAll("#formacoes-botao");
 
-listaFormacoes.forEach(formacao => {
-    const element = `
-        <a href="${formacao.url}" target="_blank"><div class="formacao-box">  ${formacao.nome} </div></a>
-    `
+let i = 0;
+const partesLista = dividirLista(listaFormacoes, 4);
+mostrarFormacoes(partesLista[i]);
 
-    formacaoContainerAjuste.innerHTML += element;
-})
+function dividirLista(lista, tamanho) {
+    const partes = [];
+    for (let i = 0; i < lista.length; i += tamanho) {
+        partes.push(lista.slice(i, i + tamanho));
+    }
+    return partes;
+}
+
+function mostrarFormacoes(lista){
+    formacaoContainerAjuste.innerHTML = "";
+
+    lista.forEach(formacao => {
+        const element = `
+            <a href="${formacao.url}" target="_blank"><div class="formacao-box">  ${formacao.nome} </div></a>
+        `
+
+        formacaoContainerAjuste.innerHTML += element;
+    })
+}
 
 botoesFormacoes.forEach(botao => {
     botao.addEventListener("click", (event) => {
         event.preventDefault();
 
         let acao = botao.innerHTML.trim();
-        console.log(acao)
         
+
         if(acao == "&gt;"){
-            alert(">")
+            try{
+                if((i+1) < partesLista.length){
+                    i++;
+                    mostrarFormacoes(partesLista[i]); 
+                }
+                
+            }
+            catch(erro){
+                i--;
+            }
         }
 
         else if(acao == "&lt;"){
-            alert("<")
+            try{
+                if(i > 0){
+                    i--;
+                    mostrarFormacoes(partesLista[i]); 
+                }
+            }
+            catch(erro){
+                i++;
+            }
         }
     })
 })
