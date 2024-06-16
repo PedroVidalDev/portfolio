921d761aae3be6f6c6a7c255956bf8ca2d11c0d7
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-16 20:06:02 -0300

    feat: criando script para percorrer lista de projetos de acordo com o botao

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -5,6 +5,7 @@ import { deletar, escrever } from "./utils/efeitoEscrever.js";
 const containerImagens = document.querySelector("#container-imagens");
 const tituloProjeto = document.querySelector("#titulo-projeto");
 const descricaoProjeto = document.querySelector("#descricao-projeto");
+const botoesProjetos = document.querySelectorAll("#projetos-botao");
 
 let i = 0;
 const partesLista = dividirLista(listaProjetos, 4);
@@ -12,6 +13,7 @@ const partesLista = dividirLista(listaProjetos, 4);
 adicionarProjetos(partesLista[i]);
 
 function adicionarProjetos(projetos){
+    containerImagens.innerHTML = "";
     projetos.forEach(projeto => {
         let divImagem = document.createElement("div");
         divImagem.className = "imagem-projeto";
@@ -29,3 +31,45 @@ function adicionarProjetos(projetos){
         containerImagens.appendChild(divImagem);
     });
 }
+
+botoesProjetos.forEach(botao => {
+    botao.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let acao = botao.innerHTML.trim();
+        
+        if(acao == "&gt;"){
+            try{
+                if((i+1) < partesLista.length){
+                    i++; 
+                } 
+                
+                else{
+                    i = 0;
+                }
+            }
+            catch(erro){
+                i--;
+                return;
+            }
+        }
+
+        else if(acao == "&lt;"){
+            try{
+                if(i > 0){
+                    i--;  
+                } 
+                
+                else{
+                    i = partesLista.length - 1;
+                }
+            }
+            catch(erro){
+                i++;
+                return
+            }
+        }
+
+        adicionarProjetos(partesLista[i]);
+    })
+});
