2f1ccaf2a8ae04968865826f7026050ae4439052
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-27 22:59:22 -0300

    feat: escondendo menu ao clickar em uma opcao

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,8 @@
 const menuIcon = document.querySelector("#menu-icon");
 const menu = document.querySelector("nav");
 
+const opcoes = menu.querySelectorAll("li");
+
 menuIcon.addEventListener("click", () => {
     if(menu.classList.contains("mostrarMenu")){
         menu.classList.remove("mostrarMenu");
@@ -12,4 +14,13 @@ menuIcon.addEventListener("click", () => {
         menu.classList.add("mostrarMenu");
     }
     
+})
+
+opcoes.forEach(opcao => {
+    opcao.addEventListener("click", () => {
+        if(menu.classList.contains("mostrarMenu")){
+            menu.classList.remove("mostrarMenu");
+            menu.classList.add("esconderMenu");
+        } 
+    })
 })
