5cb8e1eee4ded032e0cb317a98d7fb1a2478a6ff
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-02 15:12:26 -0300

    fix: corrigindo funcao de fazer requisicao para o arquivo json

--- assets/js/utils/resgatarJsonParaLista.js
+++ assets/js/utils/resgatarJsonParaLista.js
@@ -2,7 +2,7 @@ async function jsonParaLista(caminho){
 
     let lista = [];
 
-    await fetch("../assets/json/formacoes.json")
+    await fetch(caminho)
     .then(response => response.json())
     .then(data => {
         data.forEach(elemento => {
