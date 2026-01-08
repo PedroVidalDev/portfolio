a6a7b7ff188d7b3ce657fdd839aefac210a4d7a9
PedroVidalDev <pedrohvidals@gmail.com>
2026-01-08 01:31:23 -0300

    refact: implement useCountUp hook for dynamic counting in GitOverview component; replace static values with animated counts

--- src/pages/HomeSection/components/GitOverview/index.tsx
+++ src/pages/HomeSection/components/GitOverview/index.tsx
@@ -1,21 +1,26 @@
 import { useTranslation } from 'react-i18next'
 import { GitOverviewContainer, GitOverviewItem } from './styles'
+import { useCountUp } from 'hooks/useCountUp'
 
 export const GitOverview = () => {
   const { t } = useTranslation()
 
+  const experienceYears = useCountUp(3, 1250, 1)
+  const projects = useCountUp(10, 2000, 7)
+  const commits = useCountUp(3000, 2500, 2850)
+
   return (
     <GitOverviewContainer>
       <GitOverviewItem>
-        <strong>3</strong>
+        <strong>{experienceYears}</strong>
         <span>{t('home.gitOverview.experienceYears')}</span>
       </GitOverviewItem>
       <GitOverviewItem>
-        <strong>10+</strong>
+        <strong>{projects}+</strong>
         <span>{t('home.gitOverview.projects')}</span>
       </GitOverviewItem>
       <GitOverviewItem>
-        <strong>3000+</strong>
+        <strong>{commits}+</strong>
         <span>{t('home.gitOverview.commits')}</span>
       </GitOverviewItem>
     </GitOverviewContainer>
